046c294fa1a8e755b27d4ae2f2a64bddb4d47725
Your Name <deepjotsingh14@gmail.com>
2026-01-07 06:16:15 +0530

    card update

--- src/Pages/CardPages.jsx
+++ src/Pages/CardPages.jsx
@@ -14,68 +14,67 @@ const CardPages = ({ props }) => {
   }, [pokemon.url]);
 
   if (!details) return <p>Loading card...</p>;
-    const color = {
-        water: {
-            bg: "radial-gradient(circle, rgba(0,86,245,1) 0%, rgba(64,131,255,1) 41%)",
-            shadow: "0 0 4rem 4rem rgba(64,131,255,0.8)",
-        },
-        fire: {
-            bg: "  radial-gradient(circle, rgba(255,94,0,1) 0%, rgba(255,170,64,1) 45%)",
-            shadow: "0 0 4rem 4rem rgba(255,120,0,0.8)",
-        },
-        grass: {
-            bg: "  radial-gradient(circle, rgba(0,200,83,1) 0%, rgba(100,221,170,1) 45%)",
-            shadow: "0 0 4rem 4rem rgba(0,200,83,0.8)",
-        },
-        normal: {
-            bg: "  radial-gradient(circle, #ddd 0%, #bbb 45%)",
-            shadow: "0 0 4  rem 4rem rgba(0,0,0,0.2)",
-        },
-        poison:{
-            bg:"radial-gradient(circle,rgba(208, 0, 255, 1) 0%, rgba(192, 97, 255, 1) 52%)",
-            shadow:"0 0 4rem 4rem rgba(192, 97, 255, 1)"
-        }
-    };
-      const type = details.types?.[0]?.type?.name || "normal";
+  const color = {
+    water: {
+      bg: "radial-gradient(circle, rgba(0,86,245,1) 0%, rgba(64,131,255,1) 41%)",
+      shadow: "0 0 4rem 4rem rgba(64,131,255,0.8)",
+    },
+    fire: {
+      bg: "  radial-gradient(circle, rgba(255,94,0,1) 0%, rgba(255,170,64,1) 45%)",
+      shadow: "0 0 4rem 4rem rgba(255,120,0,0.8)",
+    },
+    grass: {
+      bg: "  radial-gradient(circle, rgba(0,200,83,1) 0%, rgba(100,221,170,1) 45%)",
+      shadow: "0 0 4rem 4rem rgba(0,200,83,0.8)",
+    },
+    normal: {
+      bg: "  radial-gradient(circle, #ddd 0%, #bbb 45%)",
+      shadow: "0 0 4  rem 4rem rgba(0,0,0,0.2)",
+    },
+    poison: {
+      bg: "radial-gradient(circle,rgba(208, 0, 255, 1) 0%, rgba(192, 97, 255, 1) 52%)",
+      shadow: "0 0 4rem 4rem rgba(192, 97, 255, 1)"
+    }
+  };
+  const type = details.types?.[0]?.type?.name || "normal";
 
-    // 🔹 Ab safely theme lo
-    const theme = color[type] || color.normal;
+  // 🔹 Ab safely theme lo
+  const theme = color[type] || color.normal;
   return (
- <div className="  lg:h-90 h-70 relative  bg-[#F9F8F6]  rounded-2xl lg:rounded-3xl overflow-hidden ">
+    <div className="  lg:h-90 h-70 relative  bg-[#F9F8F6]  rounded-2xl lg:rounded-3xl overflow-hidden ">
       <div className="h-full absolute z-2  p-3 flex  w-full  ">
 
-  <h1 className="font-[popbold] text-2xl first-letter:uppercase ">{details.name}</h1>
+        <h1 className="font-[popbold] text-2xl first-letter:uppercase ">{details.name}</h1>
 
       </div>
-     <div className="relative h-full rounded-3xl  w-full flex items-center justify-center">
-                <div className="absolute w-full h-full flex justify-center items-center  ">
-                    <div
-                        className="lg:h-28 lg:w-28 h-18 w-18    rounded-full  bg-cover"
-                              style={{
-                            backgroundImage: theme.bg,
-                            boxShadow: theme.shadow, 
-                            backgroundPosition: "center",
-                        }}
-                    >      
-                     </div>
-                     <div className="h-full a w-full absolute  mix-blend-overlay  "
-                     style={{
-                        backgroundImage:  `url('/noise.svg')`,
-                     }}
-                     ></div>
-                        <div className="absolute h-full w-full backdrop-blur-sm border border-white/30"  >
+      <div className="relative h-full rounded-3xl  w-full flex items-center justify-center">
+        <div className="absolute w-full h-full flex justify-center items-center  ">
+          <div
+            className="lg:h-28 lg:w-28 h-18 w-18    rounded-full  bg-cover"
+            style={{
+              backgroundImage: theme.bg,
+              boxShadow: theme.shadow,
+              backgroundPosition: "center",
+            }}
+          >
+          </div>
+          <div className="h-full a w-full absolute  mix-blend-overlay  "
+            style={{
+              backgroundImage: `url('/noise.svg')`,
+            }}
+          ></div>
+          <div className="absolute h-full w-full backdrop-blur-sm border border-white/30"  >
 
-                        </div>
-                </div>
-{/* 
-               <img
-                    src={details.sprites?.other?. home?.front_default}
-                    alt={ details.name}
-                    className="relative z-10 h-full   object-contain"
-                    draggable={false}
-                />  */}
-            </div>
- </div>
+          </div>
+        </div>
+        <img
+          src={details.sprites?.other?.home?.front_default}
+          alt={details.name}
+          className="relative z-10 h-full   object-contain"
+          draggable={false}
+        />
+      </div>
+    </div>
   );
 };
 
--- src/Pages/Na-v.jsx
+++ src/Pages/Na-v.jsx
@@ -8,28 +8,45 @@ const Nav = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!search.trim()) return;
-navigate(`/search?query=${search}`);
+    navigate(`/search?query=${search}`);
 
-    
+
   };
 
   return (
     <>
       {/* Mobile Bottom Nav */}
-      <div className="fixed bottom-0 left-0 right-0 flex justify-between p-4 lg:hidden z-50">
-        <button>
-          <img src="/home.svg" className="h-10 w-10" />
-        </button>
-        <button>
-          <img src="/game.svg" className="h-10 w-10" />
-        </button>
-        <button>
-          <img src="/user.svg" className="h-10 w-10" />
-        </button>
+      <div className="fixed bottom-0 left-0 right-0 flex flex-col justify-between gap-2 p-4 lg:hidden z-50">
+        <div>               
+             <form
+          onSubmit={handleSubmit}
+          className="flex backdrop-blur-md  border border-white/30  justify-between  border p-1 rounded-full"
+        >
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search..."
+            className="px-4 py-2  w-full   rounded  outline-none"
+          />
+          <button type="submit">
+            <img className="w-8 h-8" src="/magnifying-glass.svg" />
+          </button>
+        </form></div>
+        <div className=" w-full flex justify-evenly">
+          <button>
+            <img src="/home.svg" className="h-8 w-8" />
+          </button>
+          <button>
+            <img src="/game.svg" className="h-8 w-8" />
+          </button>
+          <button>
+            <img src="/user.svg" className="h-8 w-8" />
+          </button>
+        </div>
       </div>
 
       {/* Desktop Top Nav */}
-      <div className=" lg:flex fixed w-full justify-center    py-6 z-50">
+      <div className=" lg:flex fixed w-full justify-center  hidden  py-6 z-50">
         <div className="flex gap-12 items-center">
           <Link to="/" className="hidden lg:block">
             <img className="w-8 h-8" src="/home.svg" />
